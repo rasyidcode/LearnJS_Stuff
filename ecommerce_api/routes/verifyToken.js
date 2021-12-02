14d0ca353f58a89a49628ada6d65209fff7241d8
rasyidcode <jamil@dwebsite.net>
2021-12-02 10:42:57 +0800

    update

---
+++ ecommerce_api/routes/verifyToken.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken")
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.token
+    if (!authHeader)
+        return res.status(401).json("You are not authenticated")
+
+    const token = authHeader.split(" ")[1]
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err)
+            return res.status(401).json("Token is not valid!")
+
+        req.user = user
+        next()
+    })
+}
+
+const verifyTokenAndAuth = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user.id === req.params.id || req.user.isAdmin) {
+            next()
+        } else {
+            return res.status(403).json("You are not allowed!")
+        }
+    })
+}
+
+module.exports = { verifyToken, verifyTokenAndAuth }
